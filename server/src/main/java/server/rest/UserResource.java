a04e9e57a85164fb10af5f62eb6a9cbdb4414816
CNadjim <c.nadjim@gmail.com>
2018-05-17 22:44:09 +0200

    [#33] Fix route

--- server/src/main/java/server/rest/UserResource.java
+++ server/src/main/java/server/rest/UserResource.java
@@ -1,9 +1,12 @@
 package server.rest;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import server.dto.user.UpdateUserEmailRequest;
 import server.dto.user.UpdateUserInfoRequest;
+import server.dto.user.UpdateUserPasswordRequest;
 import server.dto.user.UserStatsResponse;
 import server.model.bettor.Player;
 import server.model.user.User;
@@ -20,17 +23,23 @@ public class UserResource {
         this.userResourceDelegate = userResourceDelegate;
     }
 
-    @RequestMapping(path = "/api/user", method = RequestMethod.PATCH)
+    @RequestMapping(path = "/api/user/info", method = RequestMethod.PATCH)
     @PreAuthorize("hasRole('ROLE_USER')")
     public ResponseEntity<User> updateUserInfo(@RequestBody UpdateUserInfoRequest user, HttpServletRequest request) {
         return userResourceDelegate.updateUser(user,request);
     }
-    /*
-    @RequestMapping(path = "/api/user", method = RequestMethod.PATCH)
+
+    @RequestMapping(path = "/api/user/email", method = RequestMethod.PATCH)
     @PreAuthorize("hasRole('ROLE_USER')")
-    public ResponseEntity<User> updateUserEmail(@RequestBody UpdateUserInfoRequest user, HttpServletRequest request) {
-        return userResourceDelegate.updateUser(user,request);
-    }*/
+    public ResponseEntity<?> updateUserEmail(@RequestBody UpdateUserEmailRequest user, HttpServletRequest request) {
+        return new ResponseEntity<>("not implemented", HttpStatus.OK);
+    }
+
+    @RequestMapping(path = "/api/user/password", method = RequestMethod.PATCH)
+    @PreAuthorize("hasRole('ROLE_USER')")
+    public ResponseEntity<?> updatePassword(@RequestBody UpdateUserPasswordRequest user, HttpServletRequest request) {
+        return new ResponseEntity<>("not implemented", HttpStatus.OK);
+    }
 
     @RequestMapping(path = "/api/user", method = RequestMethod.GET)
     @PreAuthorize("hasRole('ROLE_USER')")
