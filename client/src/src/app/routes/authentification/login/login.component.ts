de67501125978228bb722b05c5ca183b6ba0558e
CNadjim <c.nadjim@gmail.com>
2018-06-23 12:50:52 +0200

    Fix auth redirect

--- client/src/src/app/routes/authentification/login/login.component.ts
+++ client/src/src/app/routes/authentification/login/login.component.ts
@@ -38,26 +38,24 @@ export class LoginComponent implements OnInit {
     ngOnInit() {
       this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
 
-    this.loginForm = this.formBuilder.group({
-      email   : ['', [Validators.required]],
-      //email   : ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required]
-    });
+      this.loginForm = this.formBuilder.group({
+        email   : ['', [Validators.required]],
+        //email   : ['', [Validators.required, Validators.email]],
+        password: ['', Validators.required]
+      });
 
-    this.loginForm.valueChanges.subscribe(() => {
-      this.onLoginFormValuesChanged();
-    });
+      this.loginForm.valueChanges.subscribe(() => {
+        this.onLoginFormValuesChanged();
+      });
     }
 
-  onLoginFormValuesChanged()
-  {
+  onLoginFormValuesChanged() {
     for ( const field in this.loginFormErrors )
     {
       if ( !this.loginFormErrors.hasOwnProperty(field) )
       {
         continue;
       }
-
       // Clear previous errors
       this.loginFormErrors[field] = {};
 
@@ -71,8 +69,7 @@ export class LoginComponent implements OnInit {
     }
   }
 
-    onSubmit()
-    {
+    onSubmit() {
         this.loginRequest = new LoginRequestModel();
         this.loginRequest.password = this.loginForm.value.password;
         this.loginRequest.username = this.loginForm.value.email;
@@ -82,9 +79,11 @@ export class LoginComponent implements OnInit {
                     this.authService.setToken(data.token);
                     //localStorage.setItem('TOKEN_KEY', data.token);
                     this.authService.initUser().then( res =>{
-                            if (this.returnUrl){
+                            if  (this.returnUrl){
+                              console.log("go to last page");
                                 this.router.navigateByUrl(this.returnUrl);
                             }else
+                              console.log("go to profile");
                                 this.router.navigate(['/profile']);
                         }
                     );
